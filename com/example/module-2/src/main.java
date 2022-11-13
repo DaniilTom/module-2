dae7e342376d9fd0bb8a645c30d91d83e43bcf5a
DaniilTom <44425958+DaniilTom@users.noreply.github.com>
2022-11-13 20:21:38 +0300

    Update main.java

--- com/example/module-2/src/main.java
+++ com/example/module-2/src/main.java
@@ -1,7 +1,11 @@
 package com.example.module-2
 
-public class Main {
+public class Main2 {
   public static void main(String[] args) {
     System.out.println("ok");
    }
+  
+  public void print() {
+    System.out.ptinrln("from module 2"); 
+  }
 }
